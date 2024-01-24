05b9a9a5b9f7dfb18f67e2a9250e5529d1cd8fbb
oskarwires <115482671+oskarwires@users.noreply.github.com>
2024-01-24 22:24:23 +0000

    Forgot to push the updated python code!

--- test_ram.py
+++ test_ram.py
@@ -1,38 +1,63 @@
 import serial
 import time
 
-# Open the serial port
-ser = serial.Serial('COM4', 115200, timeout=1)
+BurstLength = 4
 
-def write_ram(addr, value):
+# Open the serial port
+ser = serial.Serial('COM4', 115200, timeout=0.1)
+
+def write_ram(bank, col, row, value):
+    if bank < 0 or bank > 1 or col < 0 or col > 7 or row < 0 or row > 7:
+        raise ValueError("Invalid input values. Bank, col, and row must be within their respective ranges.")
     if value >= 255:
-        raise ValueError("Can't send a value over 255")
+        raise ValueError("Can't send a value over 255 (8 bits)")
+    if value < 0:
+        raise ValueError("Can only send unsigned (positive) values")
+    addr = (bank << 6) | (col << 3) | row
     bytes_to_send = [ord('w'), addr, value]
     ser.write(bytearray(bytes_to_send))
-    time.sleep(0.01)
+    time.sleep(0.1)
+    print(f"Write: {value} to bank {bank} col {col} row {row} (addr: {addr})")
 
-def read_ram(addr):
+def read_ram(bank, col, row, BurstLength):
+    if bank < 0 or bank > 1 or col < 0 or col > 7 or row < 0 or row > 7:
+        raise ValueError("Invalid input values. Bank, col, and row must be within their respective ranges.")
+    addr = (bank << 6) | (col << 3) | row
     bytes_to_send = [ord('r'), addr]
     ser.write(bytearray(bytes_to_send))
-    time.sleep(0.01)
-    response = ser.read(ser.inWaiting())
-    if response:
-      # Convert the first byte to an integer
-      decimal_value = int.from_bytes(response, byteorder='big')
-      return decimal_value
-    else:
-      return "No response"
+    time.sleep(0.1)
     
+    responses = []
 
-write_ram(2,143)
-write_ram(13,102)
-write_ram(3,2)
-value_1 = read_ram(2)
-value_2 = read_ram(13)
-value_3 = read_ram(3)
-print(f"Value at addr 2 is: {value_1}")
-print(f"Value at addr 13 is: {value_2}")
-print(f"Value at addr 3 is: {value_3}")
+    for _ in range(BurstLength):
+        time.sleep(0.01)
+        # Read exactly one byte
+        response = ser.read(1)
+        if response:
+            decimal_value = int.from_bytes(response, byteorder='big')
+            responses.append(decimal_value)
+        else:
+            responses.append("No response")
+
+    for value in responses:
+            print(f"Read: {value} at bank {bank} col {col} row {row} (addr: {addr})")
+
+
+
+    
+ser.flushInput()
+ser.flushOutput()
+write_ram(0,0,1,143) # bank, col, row
+write_ram(1,0,2,102)
+write_ram(0,2,0,2)
+read_ram(0, 0, 1, BurstLength);
+read_ram(1, 0, 2, BurstLength);
+read_ram(0, 2, 0, BurstLength);
+time.sleep(1);
+read_ram(1, 0, 2, BurstLength);
+time.sleep(1);
+read_ram(0, 0, 1, BurstLength);
+
 
 # Close the serial port
 ser.close()
